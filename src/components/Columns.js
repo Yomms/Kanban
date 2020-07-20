884f1deb7af98a48adb9995bb2cd62d6cf3682f3
david <yoo.m.david@gmail.com>
2020-07-20 12:11:19 -0700

    initial

---
+++ src/components/Columns.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import Card from './Card.js';
+
+function Columns() {
+  return (
+  	<div>
+	    <span className='Column1'>Columns</span>
+	    <span className='Column2'>Columns2</span>
+	    <span className='Column3'>Columns3</span>
+	    <Card />
+	  </div>
+  );
+}
+
+export default Columns;
